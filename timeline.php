49b204951d391c7d4e4c7bf9b6bd9d195bdfe264
Aditya8112 <123253432+Aditya8112@users.noreply.github.com>
2023-06-09 15:36:15 +0530

    Final project.

---
+++ timeline.php
@@ -0,0 +1,254 @@
+<?php
+function time_stamp($session_time) 
+{ 
+ 
+$time_difference = time() - $session_time ; 
+$seconds = $time_difference ; 
+$minutes = round($time_difference / 60 );
+$hours = round($time_difference / 3600 ); 
+$days = round($time_difference / 86400 ); 
+$weeks = round($time_difference / 604800 ); 
+$months = round($time_difference / 2419200 ); 
+$years = round($time_difference / 29030400 ); 
+
+if($seconds <= 60)
+{
+echo"$seconds seconds ago"; 
+}
+else if($minutes <=60)
+{
+   if($minutes==1)
+   {
+     echo"one minute ago"; 
+    }
+   else
+   {
+   echo"$minutes minutes ago"; 
+   }
+}
+else if($hours <=24)
+{
+   if($hours==1)
+   {
+   echo"one hour ago";
+   }
+  else
+  {
+  echo"$hours hours ago";
+  }
+}
+else if($days <=7)
+{
+  if($days==1)
+   {
+   echo"one day ago";
+   }
+  else
+  {
+  echo"$days days ago";
+  }
+
+
+  
+}
+else if($weeks <=4)
+{
+  if($weeks==1)
+   {
+   echo"one week ago";
+   }
+  else
+  {
+  echo"$weeks weeks ago";
+  }
+ }
+else if($months <=12)
+{
+   if($months==1)
+   {
+   echo"one month ago";
+   }
+  else
+  {
+  echo"$months months ago";
+  }
+ 
+   
+}
+
+else
+{
+if($years==1)
+   {
+   echo"one year ago";
+   }
+  else
+  {
+  echo"$years years ago";
+  }
+
+}
+ 
+} 
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Welcome  To Biobook - Sign up, Log in, Post </title>
+		<link rel="stylesheet" type="text/css" href="css/timeline.css">
+	</head>
+
+<body>
+<?php include ('session.php');?>
+
+	<div id="header">
+		<div class="head-view">
+			<ul>
+				<li><a href="home.php" title="Biobook"><b>biobook</b></a></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li><a href="timeline.php" title="<?php echo $username ?>"><label class="active"><?php echo $username ?></label></a></li>
+				<li><a href="home.php" title="Home"><label>Home</label></a></li>
+				<li><a href="profile.php" title="Home"><label>Profile</label></a></li>
+				<li><a href="photos.php" title="Settings"><label>Photos</label></a></li>
+				<li><a href="logout.php" title="Log out"><button class="btn-sign-in" value="Log out">Log out</button></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="container">
+	
+		<div id="clip2">
+				<a href="updatecover.php" title="Change Cover Photo"><img src="<?php echo $row['cover_picture'] ?>"></a>
+
+
+				
+		</div>	
+		
+		<div id="left-nav">
+				
+				<div class="clip1">
+
+				<a href="updatephoto.php" title="Change Profile Picture"><img src="<?php echo $row['profile_picture'] ?>"></a>
+				</div>
+				
+				<div class="user-details">
+					<label><?php echo $firstname ?>&nbsp;<?php echo $lastname ?></label>
+					<br />
+					<b>(<?php echo $username ?>)</b>
+				</div>
+		</div>
+	
+		<div id="right-nav">
+			<h1>Update Status</h1>
+	<div>
+			<form method="post" enctype="multipart/form-data">
+				<textarea placeholder="Whats on your mind?" name="content" class="post-text" required></textarea>
+				<input type="file" name="image">
+				<button class="btn-share" name="Submit" value="Log out">Share</button>
+			</form>
+			
+<?php
+	include('includes/database.php');
+							
+							if (!isset($_FILES['image']['tmp_name'])) {
+							echo "";
+							}else{
+							$file=$_FILES['image']['tmp_name'];
+							$image = $_FILES["image"] ["name"];
+							$image_name= addslashes($_FILES['image']['name']);
+							$size = $_FILES["image"] ["size"];
+							$error = $_FILES["image"] ["error"];
+
+							if ($error > 0){
+										die("Error uploading file! Code $error.");
+									}else{
+										if($size > 10000000) //conditions for the file
+										{
+										die("Format is not allowed or file size is too big!");
+										}
+										
+									else
+										{
+
+									move_uploaded_file($_FILES["image"]["tmp_name"],"upload/" . $_FILES["image"]["name"]);			
+									$location="upload/" . $_FILES["image"]["name"];
+									$user=$_SESSION['id'];
+									$content=$_POST['content'];
+									$time=time();
+									
+									$update=mysqli_query($con," INSERT INTO post (user_id,post_image,content,created)
+									VALUES ('$id','$location','$content','$time') ");
+
+									}
+										header('location:timeline.php');
+									
+									
+									}
+							}
+?>
+			
+	</div>
+	
+		</div>
+		
+
+ 	
+		
+<?php
+	include("includes/database.php");
+			$query=mySQLi_query($con,"SELECT * from user where user_id='$id' order by user_id DESC");
+			while($row=mySQLi_fetch_array($query)){
+				$id = $row['user_id'];
+?>	
+		<div id="left-nav1">
+			<h2>Personal Info</h2>
+			<table>
+				<tr>
+					<td><label>Username</label></td>
+					<td width="20"></td>
+					<td><b><?php echo $row['username']; ?></b></td>
+				</tr>
+				<tr>
+					<td><label>Birthday</label></td>
+					<td width="20"></td>
+					<td><b><?php echo $row['birthday']; ?></b></td>
+				</tr>
+				<tr>
+					<td><label>Gender</label></td>
+					<td width="20"></td>
+					<td><b><?php echo $row['gender']; ?></b></td>
+				</tr>
+				<tr>
+					<td><label>Contact</label></td>
+					<td width="20"></td>
+					<td><b><?php echo $row['number']; ?></b></td>
+				</tr>
+				<tr>
+					<td><label>Email</label></td>
+					<td width="20"></td>
+					<td><b><?php echo $row['email']; ?></b></td>
+				</tr>
+				<tr>
+					<td><label>Image</label></td>
+					<td width="20"></td>
+					<td><img src="<?php echo $row['profile_picture']; ?>"></td>
+				</tr>
+			</table>
+		</div>
+<?php
+}
+?>
+
+
+
+</body>
+
+</html>
